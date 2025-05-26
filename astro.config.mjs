1c46fd6279f508910f40a3f30301b4f09324ea1f
AlexanderUzco <76892923+AlexanderUzco@users.noreply.github.com>
2025-05-26 15:05:03 -0400

    First Commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    output: 'server',
+    site: 'https://map-traffic-layer.github.io',
+    base: '/map-traffic-layer/',
+});
