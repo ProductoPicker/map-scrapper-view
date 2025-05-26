8009c2a216b075b69420e3204fafbe6ba4bf3e58
AlexanderUzco <76892923+AlexanderUzco@users.noreply.github.com>
2025-05-26 15:21:31 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
+    output: 'static',
     site: 'https://map-traffic-layer.github.io',
     base: '/map-traffic-layer/',
 });
